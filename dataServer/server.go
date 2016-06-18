eabfa30d1b66753885e2fa87ee4ee27fa81ddb21
Travis Lanham <tlanham@cs.stanford.edu>
2016-06-18 12:43:16 -0700

    added comments

--- dataServer/server.go
+++ dataServer/server.go
@@ -1,3 +1,9 @@
+/*
+ * File: server.go
+ * ------------------------------
+ * Implements simple server that provides the time. 
+ */
+
 package main
 
 import (
@@ -8,11 +14,17 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    // simple time server that responds to request with 
+    // the current time, prints message to server console 
+    // to acknowledge request
     fmt.Fprintf(w, "Hi, the time is %s", time.Now())
     fmt.Println("Received connection.")
 }
 
 func getPort() string {
+    // Prompts user to specify a port to host server on
+    // from 1024 (lowest port that doesn't require root 
+    // access) and 65535 (highest port number)
     for {
         fmt.Print("Enter port number to create server (1024 to 65535): ")
         var resp string
@@ -31,6 +43,9 @@ func getPort() string {
 }
 
 func main() {
+    // installs handler function for all requests 
+    // listens on user-defined port and serves requests 
+    // using the installed handler
     http.HandleFunc("/", handler)
     http.ListenAndServe(getPort(), nil)
 }
