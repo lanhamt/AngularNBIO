a012746bce300015d5ab9200b9817c3f9a46c90c
Travis Lanham <tlanham@cs.stanford.edu>
2016-06-18 02:13:30 -0700

    added local logging, config port

--- dataServer/server.go
+++ dataServer/server.go
@@ -4,14 +4,34 @@ import (
     "fmt"
     "net/http"
     "time"
+    "strconv"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi, the time is %s", time.Now())
+    fmt.Println("Received connection.")
+}
+
+func getPort() string {
+    for {
+        fmt.Print("Enter port number to create server (1024 to 65535): ")
+        var resp string
+        _, err := fmt.Scanln(&resp)
+        if err != nil {
+            continue
+        }
+        num, err := strconv.ParseInt(resp, 10, 32)
+        if err != nil {
+            continue
+        }
+        if num >= 1024 && num <= 65535 {
+            return ":" + resp
+        }
+    }
 }
 
 func main() {
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(getPort(), nil)
 }
 
